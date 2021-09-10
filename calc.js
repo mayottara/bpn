740c518f13afc64569d476286d010f9325e59853
mayottara <mayottara.1000@outlook.jp>
2021-09-10 21:02:56 +0900

    cookieテスト

--- calc.js
+++ calc.js
@@ -1,8 +1,15 @@
 (function() {
   'use strict';
   const btn = document.getElementById('today');
+  const testbtn = document.getElementById('testbtn');
+
   const mor_bpmax_ave = document.getElementById('mor_bpmax_ave')
   const mor_bpmin_ave = document.getElementById('mor_bpmin_ave')
+  const mor_pul_ave = document.getElementById('mor_pul_ave')
+  const nig_bpmax_ave = document.getElementById('nig_bpmax_ave')
+  const nig_bpmin_ave = document.getElementById('nig_bpmin_ave')
+  const nig_pul_ave = document.getElementById('nig_pul_ave')
+
   var mor_maxdata = document.getElementsByClassName('mor_max')
   var mor_mindata = document.getElementsByClassName('mor_min')
   var mor_puldata = document.getElementsByClassName('mor_pul')
@@ -14,9 +21,9 @@
 
 //cookie読み出し処理
 if(document.cookie.length > 0){                       //cookieにデータがある場合実行
-  var cookie_data = document.cookie.split(';');       //データ分割(データごとに分割)
-  var day_cookie_data = document.cookie.split('=');   //データ分割(名前とデータを分割)
-  day[0].value = day_cookie_data[1];                  //日付の1つ目にcookieを代入
+  //var cookie_data = document.cookie.split(';');       //データ分割(データごとに分割)
+  var day_cookie = document.cookie.split('=');   //データ分割(名前とデータを分割)
+  day[0].value = day_cookie[1];                  //日付の1つ目にcookieを代入
 
   for(let i = 0; i < day.length; i++){                          //日付データの数だけ繰り返す
     var tomo = new Date(day[0].value);                          //１つ目の日付データをDate形に変換
@@ -33,10 +40,27 @@ day[0].onchange = day_calc;
 
 for(let i = 0; i < mor_maxdata.length; i++){
   mor_maxdata[i].onchange = calc;
-}
-for(let i = 0; i < mor_mindata.length; i++){
   mor_mindata[i].onchange = calc;
+  mor_puldata[i].onchange = calc;
+  nig_maxdata[i].onchange = calc;
+  nig_mindata[i].onchange = calc;
+  nig_puldata[i].onchange = calc;
 }
+// for(let i = 0; i < mor_mindata.length; i++){
+//   mor_mindata[i].onchange = calc;
+// }
+// for(let i = 0; i < mor_puldata.length; i++){
+//   mor_puldata[i].onchange = calc;
+// }
+// for(let i = 0; i < nig_maxdata.length; i++){
+//   nig_maxdata[i].onchange = calc;
+// }
+// for(let i = 0; i < nig_mindata.length; i++){
+//   nig_mindata[i].onchange = calc;
+// }
+// for(let i = 0; i < nig_puldata.length; i++){
+//   nig_puldata[i].onchange = calc;
+// }
 
 /*-------------------------------------------------------------------*/
 
@@ -62,8 +86,12 @@ for(let i = 0; i < mor_mindata.length; i++){
 
 //平均値算出処理
   function calc(){                            //onchangeで呼ばれ、処理を行う関数を呼び出す
-    calc_data(mor_maxdata , mor_bpmax_ave);   //血圧の最大値の処理
-    calc_data(mor_mindata , mor_bpmin_ave);   //血圧の最小値の処理
+    calc_data(mor_maxdata , mor_bpmax_ave);   //朝の血圧の最大値の処理
+    calc_data(mor_mindata , mor_bpmin_ave);   //朝の血圧の最小値の処理
+    calc_data(mor_puldata , mor_pul_ave);     //朝の脈拍の処理
+    calc_data(nig_maxdata , nig_bpmax_ave);   //夜の血圧の最大値の処理
+    calc_data(nig_mindata , nig_bpmin_ave);   //夜の血圧の最小値の処理
+    calc_data(nig_puldata , nig_pul_ave);     //夜の脈拍の処理
   }
 
   function calc_data(data , ptn){
@@ -90,4 +118,22 @@ for(let i = 0; i < mor_mindata.length; i++){
     day[0].value = String(today.getMonth() + 1) + "/" + String(today.getDate());  //本日の日付を日付の1つ目に代入(月/日)
     day_data(day);                                                                //残りの日付を表示するため関数呼び出し
   }
+
+/*-------------------------------------------------------------------*/
+
+//ボタン(本日の日付入力)
+  testbtn.onclick = function(){
+   data_keep(day);
+  }
+
+  function data_keep(){
+    var str = 'cookie_data=';
+    for(let i = 0; i < mor_maxdata.length; i++){
+      str += mor_maxdata[i].value+','+mor_mindata[i].value+','+mor_puldata[i].value+','+
+             nig_maxdata[i].value+','+nig_mindata[i].value+','+nig_puldata[i].value+',';
+    }
+    alert(str)
+  }
+
+/*-------------------------------------------------------------------*/
 })();
